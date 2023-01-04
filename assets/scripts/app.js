5f6de5c0ede33cd7880cb62aa089e0bb2a60ab1a
Lauren Tanea <taneajoshi@gmail.com>
2023-01-05 00:10:40 +0530

    moving dom elements from one list to another and vice-versa

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,41 +1,72 @@
-class Tooltip {}
-
-class ProjectItem {
-    constructor(id) {
-        this.id = id;
-        this.connectMoreInfoButton();
-        this.connectSwitchButton();
+class DOMHepler {
+    static clearEventListners(element) {
+        const clonedElement = element.cloneNode(true);
+        element.replaceWith(clonedElement);
+        return clonedElement;
     }
-
-    connectMoreInfoButton() {
-
-    }
-
-    connectSwitchButton() {
-        const projectItemElement = document.getElementById(this.id);
-        const switchButton = projectItemElement.querySelector('button:last-of-type');
-        switchButton.addEventListener('click', )
+    static moveElement(elementId, newDestinationSelector) {
+        const element = document.getElementById(elementId);
+        const destinationElement = document.querySelector(newDestinationSelector);
+        destinationElement.append(element);
     }
 }
 
+class Tooltip {}
+
+class ProjectItem {
+  constructor(id, updateProjectListsFunction, type) {
+    this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton(type);
+  }
+
+  connectMoreInfoButton() {}
+
+  connectSwitchButton(type) {
+    const projectItemElement = document.getElementById(this.id);
+    let switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn = DOMHepler.clearEventListners(switchBtn);
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+    switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(null, this.id));
+  }
+
+  update(updateProjectListFn, type) {
+    this.updateProjectListsHandler = updateProjectListFn;
+    this.connectSwitchButton(type);
+  }
+}
+
 class ProjectList {
-    projects = [];
-    constructor(type) {
-        const prjItems = document.querySelectorAll(`#${type}-projects li`);
-        for (const prjItem of prjItems) {
-            this.projects.push(new ProjectItem(prjItem.id));
-        }
-    }
+  projects = [];
 
-    addProject() {
-
+  constructor(type) {
+    this.type = type;
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
+      );
     }
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject(project) { 
+    this.projects.push(project); 
+    DOMHepler.moveElement(project.id, `#${this.type}-projects ul`); 
+    project.update(this.switchProject.bind(this), this.type);
+  }
+
     switchProject(projectId) {
         //One way to remove the project from the list
         // const projectIndex = this.projects.findIndex(p => p.id === projectId );
         // this.projects.splice(projectIndex, 1);
 
         //Shorter alternative ->
+        this.switchHandler(this.projects.find(p => p.id === projectId));
         this.projects = this.projects.filter(p => p.id !== projectId);
     }
 }
@@ -43,8 +74,10 @@ class ProjectList {
 class App  {
     //Static method as we will call it only once, for the first time when the script loads.
     static init() {
-        const activeProjectList = new ProjectList('active');
-        const FinishedProjectList = new ProjectList('finished');
+        const activeProjectsList = new ProjectList('active');
+        const finishedProjectsList = new ProjectList('finished');
+        activeProjectsList.setSwitchHandlerFunction(finishedProjectsList.addProject.bind(finishedProjectsList));
+        finishedProjectsList.setSwitchHandlerFunction(activeProjectsList.addProject.bind(activeProjectsList));
 
     }
 }

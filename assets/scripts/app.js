77fe21cbf01a2870af5c87f1a00cdfc719bd5a6c
Lauren Tanea <taneajoshi@gmail.com>
2023-01-05 11:01:00 +0530

    added tooltip to the project more info button, handled case of multiple tooltip opening on same click

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,9 +11,35 @@ class DOMHepler {
     }
 }
 
-class Tooltip {}
+class Tooltip {
+
+  constructor (closeNotifierFunction) {
+    this.closeNotifier = closeNotifierFunction;
+  }
+
+  closeTooltip = () => {
+    this.detach();
+    this.closeNotifier();
+  }
+
+  detach() {
+    this.element.remove();
+    //If we need the older browser support, then we need to do this instead - >
+    // this.element.parentElement.removeChild(this.element);
+  }
+
+  attach() {
+    const tooltipElement = document.createElement('div');
+    tooltipElement.className = "card"; 
+    tooltipElement.textContent = "Dummy tooltip content";
+    tooltipElement.addEventListener('click', this.detach);
+    this.element = tooltipElement;
+    document.body.append(tooltipElement);
+  }  
+}
 
 class ProjectItem {
+  hasActiveTooltip = false;
   constructor(id, updateProjectListsFunction, type) {
     this.id = id;
     this.updateProjectListsHandler = updateProjectListsFunction;
@@ -21,7 +47,24 @@ class ProjectItem {
     this.connectSwitchButton(type);
   }
 
-  connectMoreInfoButton() {}
+  showMoreInfoHandler() {
+    if(this.hasActiveTooltip) {
+      return;
+    }
+
+    const tooltip = new Tooltip(()=> {
+      this.hasActiveTooltip = false;
+    });
+
+    tooltip.attach();
+    this.hasActiveTooltip = true;
+  }
+
+  connectMoreInfoButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const moreInfoBtn = projectItemElement.querySelector('button:first-of-type');
+    moreInfoBtn.addEventListener('click', this.showMoreInfoHandler);
+  }
 
   connectSwitchButton(type) {
     const projectItemElement = document.getElementById(this.id);
